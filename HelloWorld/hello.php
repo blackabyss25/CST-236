bd4a93fbd27e86f46a6aefe7fc5abd29a4b68ec9
blackabyss25 <blackabyss25@gmail.com>
2021-02-26 15:45:04 -0800

    Update hello.php

--- HelloWorld/hello.php
+++ HelloWorld/hello.php
@@ -1,2 +1,3 @@
 <?php
 echo "Hello World! This isn't my first php program, but I do suck at PHP so I need all the practice I can get... >_>";
+echo "This statement was added after as a test of it being deployed properly on Heroku.";
