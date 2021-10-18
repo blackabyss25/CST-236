5ecc814e3767f83a9ebbd1c11ea5f8480e7918fa
Kurt Newcomb <blackabyss25@gmail.com>
2021-10-17 20:07:26 -0700

    person push

---
+++ cst236/week1/activity1/Person.php
@@ -0,0 +1,82 @@
+<?php
+namespace week1\activity1;
+
+class Person
+{
+    private $name;
+    private $username;
+    private $password;
+    
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function __function($name){
+        echo "Hello my name is " . $name . "<br>";
+        $this->name = $name;
+        $this->username = "kurt";
+        $this->password = "qwer";
+    }
+    
+    public function walk(){
+        echo "walked...";
+    }
+    
+    public function formalGreeting(){
+        echo "Good day to you sir!<br>";
+    }
+    
+    public function login($a, $b){
+        if($a == $this->username && $b == $this->password){
+            echo $this->name . " has logged in successfully.<br>";
+        }
+        else {
+            echo "login failed.<br>";
+        }
+    }
+}
+
---
+++ cst236/week1/activity1/testPerson.php
@@ -0,0 +1,17 @@
+<?php
+echo "test<br>";
+
+use week1\activity1\Person;
+
+require_once 'week1/activity1/Person.php';
+
+$p = new Person("kurt");
+$p->walk();
+
+$p2 = new Person("kate");
+echo "user 1 login attempt.<br>";
+$p->login($p->getUsername(), "qwer");
+echo"<br>";
+echo "user 2 login attempt.<br>";
+$p2->login($p->getUsername(), "qwer");
+
